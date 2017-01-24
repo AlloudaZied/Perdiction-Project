4d08e60bccbc44cd85605357473ea7ea578e1322
AlloudaSendi <sendi.zied.ensi@gmail.com>
2017-01-24 11:08:35 +0100

    [Tcb-Consulting][Predict-stock-market]:Extract Features

---
+++ extactNgram/src/main/java/com/tcb_Consulting/extactNgram/extactNgram/App.java
@@ -0,0 +1,13 @@
+package com.tcb_Consulting.extactNgram.extactNgram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
